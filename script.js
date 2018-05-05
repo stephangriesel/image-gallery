b55b18befd7b9815429049994e6f39607b1d7f57
Stephan Griesel <sgriesel@gmail.com>
2018-05-05 08:23:45 +0200

    add options

---
+++ script.js
@@ -0,0 +1,5 @@
+$('[data-fancybox]').fancybox({
+    loop: false, keyboard: true, buttons: 
+    ["zoom", "thumbs", "fullScreen", "share", "slideShow", "close"], 
+    animationEffect:"zoom-in-out", transitionEffect:"zoom-in-out"
+});
